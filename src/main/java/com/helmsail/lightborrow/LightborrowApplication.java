c728dbcf2bd6385d6bf8e833837284f7abe73587
hejye <1959365428@qq.com>
2026-06-24 12:54:05 +0800

    项目初始化

---
+++ src/main/java/com/helmsail/lightborrow/LightborrowApplication.java
@@ -0,0 +1,13 @@
+package com.helmsail.lightborrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightborrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightborrowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helmsail/lightborrow/LightborrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helmsail.lightborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightborrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
